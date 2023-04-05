8521011f64b87ff06718c177ab9454b0bd0b947d
HiroyukiUrata <hilloween1031@gmail.com>
2023-04-05 16:06:04 +0900

    123

--- next.config.js
+++ next.config.js
@@ -3,4 +3,13 @@ const webpack = require('webpack');
 module.exports = {
   target: 'serverless',
   experimental: { nftTracing: true },
+  reactStrictMode: true,
+  webpack: config => {
+  config.plugins.push(
+    new webpack.DefinePlugin({
+    CESIUM_BASE_URL: JSON.stringify('cesium'),
+    }),
+  );
+  return config;
+  }
 }
