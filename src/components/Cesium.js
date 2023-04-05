f0ca03ce18f2e75abee81ea62b92a426fa44b6e9
HiroyukiUrata <hilloween1031@gmail.com>
2023-04-05 21:44:17 +0900

    good

--- src/components/Cesium.js
+++ src/components/Cesium.js
@@ -1,7 +1,16 @@
-import { Viewer } from 'resium'
-// Vercelへのデプロイがうまくいかねぇ
+import { Entity, Viewer,BoxGraphics } from 'resium'
+import { Cartesian3,Color } from 'cesium'
+
 	export default function Cesium() {
 	  return (
-		<Viewer full />
+		<Viewer>
+			<Entity
+			name="BoxGraphics"
+			description="BoxGraphics!!"
+			position={Cartesian3.fromDegrees(0.0707383, 40.7117244, 100)}>
+			<BoxGraphics material={Color.RED} dimensions={new Cartesian3(400000.0, 300000.0, 500000.0)} />
+
+			</Entity>
+		</Viewer>
 	  )
 	}
